c146d9d744a38eaa816ca393822024783f925f6d
Test Testic <testtestic@test.com>
2020-06-20 11:01:04 +0200

    pushing finished frontend project withouth backend calls

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const rank = () => {
+    return(
+       <div>
+         <div className="white f3">
+            {'Matej, your current rank is...'}
+         </div>
+         <div className="white f1">
+            {'#5'}
+         </div>
+       </div>
+    )
+}
+
+export default rank;
